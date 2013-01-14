7b85a7bb76773191c0cedca0a9efca231cbdcfdf
Edwin De La Cruz <edwinspire@gmail.com>
2013-01-14 07:31:23 -0500

    Combobox Tipo de telefono en usmsm_contacts habilitado

--- bin/lnx/rootweb/usms_contacts.js
+++ bin/lnx/rootweb/usms_contacts.js
@@ -318,6 +318,7 @@ CP.dijit.Phone.set('value', dataxml.getStringB64(i, "phone"));
 CP.dijit.Ext.set('value', dataxml.getStringB64(i, "phone_ext"));
 CP.dijit.Ubi.set('value', dataxml.getNumber(i, "ubiphone"));
 CP.dijit.Provider.set('value', dataxml.getString(i, "idprovider"));
+CP.dijit.Type.set('value', dataxml.getString(i, "typephone"));
 CP.dijit.Note.set('value', dataxml.getStringB64(i, "note"));
 CP.dijit.Address.set('value', dataxml.getStringB64(i, "address"));
 CP.dijit.GeoX.set('value', dataxml.getFloat(i, "geox"));
@@ -343,7 +344,7 @@ var Objeto = this;
 
   var xhrArgs = {
     url: "usms_phonetable_xml",
- content: {idcontact:GlobalObject.IdContact, idphone: CP.IdPhone, enable: CP.dijit.Enable.get('checked'), phone: CP.dijit.Phone.get('value'), phone_ext: CP.dijit.Ext.get('value'), ubiphone: CP.dijit.Ubi.get('value'), idprovider: CP.dijit.Provider.get('value'), note: CP.dijit.Note.get('value'), address: CP.dijit.Address.get('value'), geox: CP.dijit.GeoX.get('value'), geoy: CP.dijit.GeoY.get('value'), ts: CP.ts},
+ content: {idcontact:GlobalObject.IdContact, idphone: CP.IdPhone, enable: CP.dijit.Enable.get('checked'), phone: CP.dijit.Phone.get('value'), phone_ext: CP.dijit.Ext.get('value'), typephone: CP.dijit.Type.get('value'), ubiphone: CP.dijit.Ubi.get('value'), idprovider: CP.dijit.Provider.get('value'), note: CP.dijit.Note.get('value'), address: CP.dijit.Address.get('value'), geox: CP.dijit.GeoX.get('value'), geoy: CP.dijit.GeoY.get('value'), ts: CP.ts},
     handleAs: "xml",
     load: function(datass){
 
