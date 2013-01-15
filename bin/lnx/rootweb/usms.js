9f063548d5f4a56e8b111c5bddffaadc2e134661
Edwin De La Cruz <edwinspire@gmail.com>
2013-01-15 03:46:12 -0500

    Se elimina funciones innecesarias de usms.js

--- bin/lnx/rootweb/usms.js
+++ bin/lnx/rootweb/usms.js
@@ -62,71 +62,9 @@ myDialog.show();
 }
 
 
-function ShowLoading(){
-var Contenedor = dojo.byId("loading_img");
-Contenedor.show();
-}
-
-function HideLoading(){
-var Contenedor = dojo.byId("loading_img");
-Contenedor.hide();
-}
-
-
 function DialogNoImplemented(){
 alert('No implementado');
 }
-
-function SetContentMaster(page){
-alert(page);
-//var Loading = dojo.byId("loading_img");
-var Contenedor = dojo.byId("MasterContent");
-
-dojo.style(Contenedor, "display", "none");
-//dojo.style(Loading, "display", "block");
-
-dojo.attr(Contenedor, "data", page);
-
-dojo.style(Contenedor, "display", "block");
-dojo.style(Contenedor, "width", "100%");
-
-// Ajustamos al alto de la pagina
-var espacio_iframe = 478;
-var sizeheader = 78;
-if (window.innerHeight){ 
-   //navegadores basados en mozilla 
-   espacio_iframe = window.innerHeight - sizeheader; 
-}else{ 
-   if (document.body.clientHeight){ 
-      	//Navegadores basados en IExplorer, es que no tengo innerheight 
-      	espacio_iframe = document.body.clientHeight - sizeheader;
-   }
-}
-dojo.style(Contenedor, "height", espacio_iframe+'px');
-//propiedadesPantalla();
-//dojo.style(Contenedor, "height", "auto");
-//dojo.style(Loading, "display", "none");
-}
-
-
-
-
-function PageStatus(){
-SetContentMaster('usms_status.html');
-/*
-var Loading = dojo.byId("loading_img");
-
-//var content = dijit.byId("MasterContent");
-//content.setContent('<embed style= "width: 100%; height: 90%;" src="usms_status.html"></embed>');
-var Contenedor = dojo.byId("MasterContent");
-dojo.style(Contenedor, "display", "none");
-dojo.style(Loading, "display", "block");
-dojo.attr(Contenedor, "src", "usms_status.html");
-dojo.style(Contenedor, "display", "block");
-dojo.style(Loading, "display", "none");
-*/
-}
-
 
 function DialogAbout(){
 // Create a new instance of dijit.Dialog
