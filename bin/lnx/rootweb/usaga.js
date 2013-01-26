b79a12b03ac394d209f87dcc46635a0f94010b2f
Edwin De La Cruz <edwinspire@gmail.com>
2013-01-26 12:54:43 -0500

    Cambios en las direcciones de las paginas

--- bin/lnx/rootweb/usaga.js
+++ bin/lnx/rootweb/usaga.js
@@ -12,65 +12,65 @@ var h = dojo.window.getBox().h-dojo.style('idHeader', 'height')-dojo.style('idme
 ContentPage.size("100%", h+"px");
 }
 
-dojo.connect(dojo.byId('opensaga_menuitem_login'), 'onclick', function(){
+dojo.connect(dojo.byId('usaga_menuitem_login'), 'onclick', function(){
 ContentPage.url("opensagalogin.html");
 });
 
-dojo.connect(dojo.byId('opensaga_menuitem_logout'), 'onclick', function(){
+dojo.connect(dojo.byId('usaga_menuitem_logout'), 'onclick', function(){
 ContentPage.url("opensagalogout.html");
 });
 
-dojo.connect(dojo.byId('opensaga_menuitem_eventmonitor'), 'onclick', function(){
+dojo.connect(dojo.byId('usaga_menuitem_eventmonitor'), 'onclick', function(){
 ContentPage.url("opensagaeventmonitor.html");
 });
 
-dojo.connect(dojo.byId('opensaga_menuitem_accounts'), 'onclick', function(){
-ContentPage.url("opensaga_account.html");
+dojo.connect(dojo.byId('usaga_menuitem_accounts'), 'onclick', function(){
+ContentPage.url("usaga_account.html");
 });
 
-dojo.connect(dojo.byId('opensaga_menuitem_maintenance'), 'onclick', function(){
-ContentPage.url("opensaga_maintenance.html");
+dojo.connect(dojo.byId('usaga_menuitem_maintenance'), 'onclick', function(){
+ContentPage.url("usaga_maintenance.html");
 });
 
 
 
-dojo.connect(dojo.byId('opensaga_menuitem_admins'), 'onclick', function(){
+dojo.connect(dojo.byId('usaga_menuitem_admins'), 'onclick', function(){
 alert('En construccion');
 });
 
-dojo.connect(dojo.byId('opensaga_menuitem_groups'), 'onclick', function(){
-ContentPage.url("opensaga_groups.html");
+dojo.connect(dojo.byId('usaga_menuitem_groups'), 'onclick', function(){
+ContentPage.url("usaga_groups.html");
 });
 
-dojo.connect(dojo.byId('opensaga_menuitem_formatNotif'), 'onclick', function(){
-ContentPage.url("opensaga_FormatNotif.html");
+dojo.connect(dojo.byId('usaga_menuitem_formatNotif'), 'onclick', function(){
+ContentPage.url("usaga_FormatNotif.html");
 });
 
-dojo.connect(dojo.byId('opensaga_menuitem_modelpanel'), 'onclick', function(){
-ContentPage.url("opensaga_panelmodel.html");
+dojo.connect(dojo.byId('usaga_menuitem_modelpanel'), 'onclick', function(){
+ContentPage.url("usaga_panelmodel.html");
 });
 
-dojo.connect(dojo.byId('opensaga_menuitem_eventTypes'), 'onclick', function(){
-ContentPage.url("opensaga_eventtypes.html");
+dojo.connect(dojo.byId('usaga_menuitem_eventTypes'), 'onclick', function(){
+ContentPage.url("usaga_eventtypes.html");
 });
 
-dojo.connect(dojo.byId('opensaga_menuitem_keywords'), 'onclick', function(){
-ContentPage.url("opensaga_keywords.html");
+dojo.connect(dojo.byId('usaga_menuitem_keywords'), 'onclick', function(){
+ContentPage.url("usaga_keywords.html");
 });
 
-dojo.connect(dojo.byId('opensaga_menuitem_usms'), 'onclick', function(){
+dojo.connect(dojo.byId('usaga_menuitem_usms'), 'onclick', function(){
 window.open('usms.html','_blank');
 });
 
-dojo.connect(dojo.byId('opensaga_menuitem_aboutopensaga'), 'onclick', function(){
+dojo.connect(dojo.byId('usaga_menuitem_aboutopensaga'), 'onclick', function(){
 alert('En construccion');
 });
 
-dojo.connect(dojo.byId('opensaga_menuitem_tutoOpenSAGA'), 'onclick', function(){
+dojo.connect(dojo.byId('usaga_menuitem_tutoOpenSAGA'), 'onclick', function(){
 alert('En construccion');
 });
 
-dojo.connect(dojo.byId('opensaga_menuitem_tutouSMS'), 'onclick', function(){
+dojo.connect(dojo.byId('usaga_menuitem_tutouSMS'), 'onclick', function(){
 alert('En construccion');
 });
 
