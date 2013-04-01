8d51a0677af46fbda56395440b61225e5c1870a2
Edwin De La Cruz <edwinspire@gmail.com>
2013-04-01 03:16:10 -0500

    Se cambia geolocalizacion de GoogleMaps por OpenStreetMaps

--- bin/lnx/rootweb/lib/custom/usms/waddress.js
+++ bin/lnx/rootweb/lib/custom/usms/waddress.js
@@ -57,8 +57,16 @@ f9: t.idf9.get('value'),
 f10: t.idf10.get('value'),
 ts: t.ts,
 idlocation: t.idlocation,
-geolocation: 'http://maps.google.es/maps?ll='+t.idgeox.get('value')+','+t.idgeoy.get('value')+'&z=10'
+geolocation: 'http://www.openstreetmap.org/?lat='+t.idgeox.get('value')+'&amp;lon='+t.idgeoy.get('value')+'&amp;zoom=17&amp;layers=M&amp;mlat='+t.idgeox.get('value')+'&amp;mlon='+t.idgeoy.get('value')
 };
+
+/*
+Para Google-Maps
+geolocation: 'http://maps.google.es/maps?ll='+t.idgeox.get('value')+','+t.idgeoy.get('value')+'&z=10'
+
+Para OpenStreeMaps
+<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=-79.006022,-2.899094,-79.002938,-2.895729&amp;layer=mapnik&amp;marker=-2.89783,-79.00502" style="border: 1px solid black"></iframe><br /><small><a href="http://www.openstreetmap.org/?lat=-2.8974115&amp;lon=-79.00448&amp;zoom=17&amp;layers=M&amp;mlat=-2.89783&amp;mlon=-79.00502">View Larger Map</a></small>
+*/
 
 },
 load: function(id){
