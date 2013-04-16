8725fc77da408d4e0b4703ec24515cfe82a6092e
Edwin De La Cruz <edwinspire@gmail.com>
2013-04-16 01:14:39 -0500

    Licencia LGPL v3

--- serial_os.c
+++ serial_os.c
@@ -1,23 +1,27 @@
-// 
-//  serial_os.vala
-//  
+//
+//
 //  Author:
-//       Edwin De La Cruz <edwinspire@gmail.com>
-//  
+//       Edwin De La Cruz <admin@edwinspire.com>
+//
 //  Copyright (c) 2011 edwinspire
-// 
+//  Web Site http://edwinspire.com
+//
+//  Quito - Ecuador
+//
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 //#define LINUX_COM
 //#define WINDOWS_COM
 
