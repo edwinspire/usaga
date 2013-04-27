af2d02987ae5a3ce898fddd99e27af03b23ae41f
Edwin De La Cruz <edwinspire@gmail.com>
2013-04-27 06:35:46 -0500

    Textos de validacion en uSMS > Contactos

--- bin/lnx/rootweb/usms_contacts.js
+++ bin/lnx/rootweb/usms_contacts.js
@@ -181,9 +181,9 @@ FormContact.dijit.reset();
 SaveForm: function(){
 
 var Objeto = this;
-Objeto.dijit.Form.validate();
+
 if(Objeto.dijit.Form.validate()){
-alert('OK validp');
+
   var xhrArgs = {
     url: "contacts_table_edit.usms",
  content: {idcontact: GlobalObject.IdContact, enable: FormContact.dijit.Enable.get('checked'), title: FormContact.dijit.Title.get('value'), firstname: FormContact.dijit.Firstname.get('value'), lastname: FormContact.dijit.Lastname.get('value'), birthday: dojo.date.locale.format(FormContact.dijit.Birthday.get('value'), {datePattern: "yyyy-MM-dd", selector: "date"}), gender: FormContact.dijit.Gender.get('value'), typeofid: FormContact.dijit.IdentificationType.get('value'), identification: FormContact.dijit.Identification.get('value'), web: FormContact.dijit.Web.get('value'), email1: FormContact.dijit.email1.get('value'), email2: FormContact.dijit.email2.get('value'), note: FormContact.dijit.Note.get('value'), ts: FormContact.ts},
@@ -213,8 +213,6 @@ NotifyMSG.setText(error);
   }
 
   var deferred = dojo.xhrPost(xhrArgs);
-}else{
-alert('No valida');
 }
 
 //return Objeto;
