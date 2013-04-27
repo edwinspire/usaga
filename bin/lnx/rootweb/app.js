c1526148e2cd183e76d00f1c056e456e9afe5b99
Edwin De La Cruz <edwinspire@gmail.com>
2013-04-27 07:27:36 -0500

    Iniciando con rama develop

--- bin/lnx/rootweb/app.js
+++ bin/lnx/rootweb/app.js
@@ -5,6 +5,7 @@
 require(["dojo/ready"], function(ready){
      ready(function(){
          // logic that requires that Dojo is fully initialized should go here
-
+// A partir de este punto todos los cambios se hará desde la rama develop
+// Para hacer alguna correccion en un bug seria mejor crear una rama a parte
      });
 });
