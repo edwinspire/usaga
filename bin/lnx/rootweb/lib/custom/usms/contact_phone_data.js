c4652a51447b69223a3b2536705c91463c9876e0
Edwin De La Cruz <edwinspire@gmail.com>
2013-04-30 01:33:00 -0500

    Se agrega el modulo contact_phone_data, terminada programacion del modulo pero aun no se lo ha incluido.

--- bin/lnx/rootweb/lib/custom/usms/contact_phone_data.js
+++ bin/lnx/rootweb/lib/custom/usms/contact_phone_data.js
@@ -1,11 +1,205 @@
 define(['dojo/_base/declare',
 'dijit/_Widget',
 'dijit/_Templated',
-'dojo/text!./contact_phone_data.html'
-],function(declare,_Widget,_Templated,templateString){
+'dojo/text!./contact_phone_data.html',
+'dojo/request', 'jspire/request/Xml', 
+'widgets/widgetspire/tooltipdialogconfirmation'
+],function(declare,_Widget,_Templated,templateString, request, RXml){
 
  return declare('usms.contact_phone_data',[ _Widget, _Templated], {
        widgetsInTemplate:true,
-       templateString:templateString   
+       templateString:templateString,
+_id: 0,
+_idcontact: 0,
+_ts: "",
+_idaddress: 0,
+reset: function(){
+t = this;
+t._id = 0;
+t._idcontact = 0;
+t._ts = "1990-01-01";
+t._idaddress = 0;
+t.Formulario.reset();
+},
+postCreate: function(){
+var t = this;
+
+t.DialogDelete.setowner(t.id_delete.id, 'onclick').on('onok', function(){
+t._Delete();
+});
+
+t.id_new.on('Click', function(){
+t._id = 0;
+t._Load();
+});
+
+t.id_save.on('Click', function(){
+t._Save();
+});
+
+},
+Load: function(idcontact_, idphone_){
+this._idcontact = idcontact_;
+this._id = idphone_;
+this.reset();
+this._Load();
+},
+_Load: function(){
+var t = this;
+if(t._id > 0){
+            // Request the text file
+            request.get("getphonebyid_xml.usms", {
+            // Parse data from xml
+	query: {idcontact: t._idcontact, idphone: t._id},
+            handleAs: "xml"
+        }).then(
+                function(response){
+var d = new RXml.getFromXhr(response, 'row');
+
+numrows = d.length;
+if(numrows > 0){
+var i = 0;
+t.Enable.set('checked', d.getBool(i, "enable"));
+t.Phone.set('value', d.getStringFromB64(i, "phone"));
+t.PhoneExt.set('value', d.getStringFromB64(i, "phone_ext"));
+
+t.TypePhone.set('value', d.getNumber(i, "typephone"));
+t.UbiPhone.set('value', d.getNumber(i, "ubiphone"));
+t.Provider.set('value', d.getString(i, "idprovider"));
+t.Note.set('value', d.getStringFromB64(i, "note"));
+t._ts = d.getStringFromB64(i, "ts");
+t._idaddress = d.getNumber(i, "idaddress");
+t._idcontact = d.getNumber(i, "idcontact");
+t._id = d.getNumber(i, "idphone");
+}else{
+t.reset();
+}
+
+t.emit('onloadphone', {idcontact: t._idcontact, idphone: t._id, idaddress: t._idaddress});
+
+                },
+                function(error){
+                    // Display the error returned
+t.reset();
+t.emit('onloadphone',  {idcontact: 0, idaddress: 0, idphone: 0});
+t.emit('onnotify', {msg: error});
+                }
+            );
+}else{
+t.reset();
+t.emit('onloadphone',  {idcontact: 0, idaddress: 0, idphone: 0});
+}
+
+},
+
+values: function(){
+var t = this;
+return {
+idcontact: t._idcontact, 
+idphone: t._id,
+enable: t.Enable.get('checked'), 
+phone: t.Phone.get('value'), 
+phone_ext: t.PhoneExt.get('value'), 
+typephone: t.TypePhone.get('value'), 
+ubiphone: t.UbiPhone.get('value'), 
+idprovider: t.Provider.get('value'), 
+note: t.Note.get('value'), 
+ts: t._ts,
+idaddress: t._idaddress
+}
+},
+
+_Save: function(){
+
+var t = this;
+
+if(t.Formulario.validate()){
+
+            // Request the text file
+            request.post("phonetable_xml.usms", {
+            // Parse data from xml
+	data: t.values(),
+            handleAs: "xml"
+        }).then(
+                function(response){
+
+var xmld = new RXml.getFromXhr(response, 'row');
+
+if(xmld.length > 0){
+
+t._id = xmld.getInt(0, 'outreturn');
+t.emit('onnotify', {msg: xmld.getStringFromB64(0, 'outpgmsg')});
+}else{
+t.reset();
+}
+t.emit('onsavephone', {idcontact: t._id});
+t._Load();
+
+                },
+                function(error){
+                    // Display the error returned
+t.emit('onnotify', {msg: error});
+t.reset();
+t._Load();
+                }
+            );
+
+}else{
+t.emit('onnotify', {msg: 'Los datos no han sido completados correctamente'});
+}
+
+//return Objeto;
+},
+_Delete: function(){
+// Internamente postgres elimina automaticamente es idaddress
+var t = this;
+
+if(t._id > 0){
+            // Request the text file
+            request.post("phonetable_xml.usms", {
+            // Parse data from xml
+	data: {idphone: t._id*-1},
+            handleAs: "xml"
+        }).then(
+                function(response){
+
+var xmld = new RXml.getFromXhr(response, 'row');
+
+if(xmld.length > 0){
+if(0 == Math.abs(xmld.getInt(0, 'outreturn'))){
+// Fue borrado correctamente
+t.emit('ondeletephone', {idcontact: t._id, idaddress: t._idaddress});
+t.reset();
+}
+t.emit('onnotify', {msg: xmld.getStringFromB64(0, 'outpgmsg')});
+}else{
+t.reset();
+}
+
+t._Load();
+
+                },
+                function(error){
+                    // Display the error returned
+t.emit('onnotify', {msg: error});
+t.reset();
+t._Load();
+                }
+            );
+
+}else{
+t.emit('onnotify', {msg: 'No ha seleccionado un contacto para eliminar'});
+}
+
+//return Objeto;
+}
+
+
+
+
+
+
+
+   
 });
 });
