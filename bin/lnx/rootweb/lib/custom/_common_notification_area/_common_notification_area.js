c8c9f9dd0f24022d6259dff195e15f19275c75d7
Edwin De La Cruz <edwinspire@gmail.com>
2013-05-15 02:35:08 -0500

    Se hace las notificaciones de usuario semitransparentes

--- bin/lnx/rootweb/lib/custom/_common_notification_area/_common_notification_area.js
+++ bin/lnx/rootweb/lib/custom/_common_notification_area/_common_notification_area.js
@@ -14,7 +14,7 @@ postCreate: function(){
 },
 setText: function(t){
  var node = domConstruct.create("span");
-node.innerHTML = '<div data-dojo-attach-point="notify_" style="margin: 3px; border: 1px solid #FF8635; border-radius: 6px; -moz-border-radius: 6px; width: 206px; background-color: transparent; background-image: -webkit-gradient(linear, left top, left bottom, from(white), to(#f3b71f)); background-image: -o-linear-gradient(white, #f3b71f); background-image: -ms-linear-gradient(white, #f3b71f); background-image: -moz-linear-gradient(white, #f3b71f); background-image: -webkit-linear-gradient(white, #f3b71f); background-image: linear-gradient(white, #f3b71f); text-align: center; height: auto;">     <label style="color: #553204;">'+t+'</label>   </div>';
+node.innerHTML = '<div data-dojo-attach-point="notify_" style="opacity:0.75; filter:alpha(opacity=75);  margin: 3px; border: 1px solid #FF8635; border-radius: 6px; -moz-border-radius: 6px; width: 206px; background-color: transparent; background-image: -webkit-gradient(linear, left top, left bottom, from(white), to(#f3b71f)); background-image: -o-linear-gradient(white, #f3b71f); background-image: -ms-linear-gradient(white, #f3b71f); background-image: -moz-linear-gradient(white, #f3b71f); background-image: -webkit-linear-gradient(white, #f3b71f); background-image: linear-gradient(white, #f3b71f); text-align: center; height: auto;">     <label style="color: #553204;">'+t+'</label>   </div>';
 
   domConstruct.place(node, this.container);
 
