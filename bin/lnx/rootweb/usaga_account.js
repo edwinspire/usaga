5ef6e7ffc0ae2743661ba2d8c7040f642d24009d
Edwin De La Cruz <edwinspire@gmail.com>
2013-05-17 03:57:18 -0500

    uSAGA - Abonados > Contactos: Tabla de contactos ya carga los datos. Parcialmente funcional.

--- bin/lnx/rootweb/usaga_account.js
+++ bin/lnx/rootweb/usaga_account.js
@@ -5,19 +5,21 @@
 require(["dojo/ready",
 "dojo/dom-style",
 "dojo/window",
-"dojo/on"
-
-], function(ready, domStyle, dojoWindow,dojoOn){
+"dojo/on",
+"jspire/Gridx",
+'dojo/request',
+'jspire/request/Xml'
+], function(ready, domStyle, dojoWindow,dojoOn, jsGridx, R, RXml){
      ready(function(){
 
  dijit.byId('id_account_titlebar').set('label', 'Abonados');
      
 var NotifyArea = dijit.byId('id_notify_area');  
-
 var Account = dijit.byId('id_account_basic_data');
-
 var Location = dijit.byId('id_account_location_widget');
 var LocationMB = dijit.byId('id_account_location_menubar');  
+var GridxA = dijit.byId('id_account_contact_gridx');  
+
 
 Location.on('notify_message', function(m){
 NotifyArea.notify({message: m.message});
@@ -63,6 +65,7 @@ Account.on('onloadaccount', function(x){
 if(x.idaccount > 0){
 Account.DisabledContentPanes(false);
 Location.set('idaddress', x.idaddress);
+GridxA.Load();
 }else{
 Account.DisabledContentPanes(true);
 }
@@ -78,6 +81,93 @@ dijit.byId('ContentPaneUsers').attr('disabled',  disabled);
 dijit.byId('ContentPaneEventos').attr('disabled',  disabled);
 }
 
+
+// ### SECCION CONTACTOS ###
+	if (GridxA) {
+// Captura el evento cuando se hace click en una fila
+dojo.connect(GridxA, 'onRowClick', function(event){
+//AC.ResetOnSelectContact();
+//var id = this.cell(event.rowId, 1, true).data();
+//AC.LoadFormContact(id);
+});
+		GridxA.setColumns([
+			{field:"idcontact", name: "idcontact", width: '0%'},
+			{field:"enable_as_contact", name: "*", width: '20px', editable: true, editor: "dijit.form.CheckBox", editorArgs: jsGridx.EditorArgsToCellBooleanDisabled, alwaysEditing: true},
+			{field:"priority", name: "priority", width: '20px'},
+			{field:"name", name: "nombre", width: '150px'},
+			{field:"appointment", name: "Designacion"}
+		]);
+GridxA.startup();
+}
+
+GridxA.Clear= function(){
+	id_account_contact_store.clearOnClose = true;
+	id_account_contact_store.data = {identifier: "unique_id", items: []};
+	id_account_contact_store.close();
+GridxA.store = null;
+GridxA.setStore(id_account_contact_store);
+}
+
+GridxA.Load = function(){
+//this.ResetOnSelectContact();
+if(Account.Id > 0){
+
+   R.get('getaccountcontactsgrid.usaga', {
+		query: {idaccount: Account.Id},
+            // Parse data from xml
+            handleAs: "xml"
+        }).then(
+                function(response){
+var d = new RXml.getFromXhr(response, 'row');
+numrows = d.length;
+
+var myData = {identifier: "idcontact", items: []};
+
+
+if(numrows > 0){
+
+var i = 0;
+while(i<numrows){
+myData.items[i] = {
+idcontact: d.getNumber(i, "idcontact"), 
+enable_as_contact: d.getBool(i, "enable_as_contact"),
+priority: d.getNumber(i, "prioritycontact"),    
+name: d.getStringFromB64(i, "lastname")+' '+d.getStringFromB64(i, "firstname"),
+appointment: d.getStringFromB64(i, "appointment")
+};
+
+i++;
+}
+
+
+}
+
+	id_account_contact_store.clearOnClose = true;
+	id_account_contact_store.data = myData;
+	id_account_contact_store.close();
+
+		GridxA.store = null;
+		GridxA.setStore(id_account_contact_store);
+
+//t.emit('onloadaccount', {idaccount: t.Id, idaddress: t._idaddress}); 
+//t.emit('notify_message', {message: t.account_select.get('displayedValue')+' cargado'}); 
+                },
+                function(error){
+                    // Display the error returned
+console.log(error);
+//t.emit('onloadaccount', {idaccount: 0, idaddress: 0}); 
+//t.emit('notify_message', {message: error}); 
+                }
+            );
+
+
+}else{
+this.Clear();
+}
+
+
+return this;
+},
 
 
 
