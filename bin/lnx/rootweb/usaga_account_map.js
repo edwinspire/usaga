df8faa83326013e1938004f0a51e6d676430d009
Edwin De La Cruz <edwinspire@gmail.com>
2013-05-19 17:53:20 -0500

    Se habilita uSAGA > Abonados > Mapa: Funciona con OpenLayers

--- bin/lnx/rootweb/usaga_account_map.js
+++ bin/lnx/rootweb/usaga_account_map.js
@@ -1,22 +1,57 @@
 
-require(["dojo/ready", "dojox/geo/openlayers/Map"], function(ready, Map){
+require(["dojo/ready", "dojox/geo/openlayers/Map", 'dojo/request', 'jspire/request/Xml'
+], function(ready, Map, R, RXml){
 
   ready(function(){
+
+var IdAccount = dojo.byId('map').getAttribute('data-usaga-idaccount');
+
     var map = new Map("map");
     // This is New York location
-    var ny = {
+    var GeoPosition = {
       longitude : -78.3983,
       latitude : -0.2074 
     };
-    // fit to New York with 0.1 degrees extent
+
+if(IdAccount > 0){
+   R.get('fun_view_account_location_byid_xml.usaga', {
+		query: {idaccount: IdAccount},
+            // Parse data from xml
+            handleAs: "xml"
+        }).then(
+                function(response){
+var d = new RXml.getFromXhr(response, 'row');
+numrows = d.length;
+
+if(numrows > 0){
+GeoPosition.latitude = d.getFloat(0, "geox");
+GeoPosition.longitude = d.getFloat(0, "geoy");
+}
+
     map.fitTo({
-      position : [ ny.longitude, ny.latitude ],
-      extent : 0.1
+      position : [ GeoPosition.longitude, GeoPosition.latitude ],
+      extent : 0
     });
 
+map.getOLMap().zoomTo(50); 
+//alert(GeoPosition.longitude+' >>> '+ GeoPosition.latitude);
+                },
+                function(error){
+                    // Display the error returned
+console.log(error);
+//t.emit('notify_message', {message: error}); 
+                }
+            );
 
-map.getOLMap().zoomTo(90); 
-alert(dojo.byId('map').getAttribute('data-usaga-idaccount'))
+
+}else{
+    // fit to New York with 0.1 degrees extent
+    map.fitTo({
+      position : [ GeoPosition.longitude, GeoPosition.latitude ],
+      extent : 0.1
+    });
+}
+
 
 
 
