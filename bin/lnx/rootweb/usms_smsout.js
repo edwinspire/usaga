1984ac7bda9b0ec25798704af385e0c060d4c0c5
Edwin De La Cruz <edwinspire@gmail.com>
2013-05-30 08:11:06 -0500

    uSMS > SMS > Salientes: Implementando

--- bin/lnx/rootweb/usms_smsout.js
+++ bin/lnx/rootweb/usms_smsout.js
@@ -24,13 +24,13 @@ require(["dojo/ready",
          // logic that requires that Dojo is fully initialized should go here
 
 dojo.connect(dojo.byId('send'), 'onclick', function(){
-myGridX.Load();
+LoadGrid();
 });
 
-jsDTb.addGetDateFunction(dijit.byId('fstart'));
-jsDTb.addGetDateFunction(dijit.byId('fend'));
 
-
+var ObjectTable = {
+RowSeleted: 0 
+} 
 
 
 	var myGridX = dijit.byId("idgridxtable");
@@ -40,20 +40,33 @@ jsDTb.addGetDateFunction(dijit.byId('fend'));
 		// Optionally change column structure on the grid
 		myGridX.setColumns([
 
-			{field:"idsmsin", name: "id"},
+			{field:"idsmsout", name: "id"},
 			{field:"dateload", name: "dateload", width: "5%"},
 			{field:"idprovider", name: "idprovider"},
+			{field:"idsmstype", name: "idsmstype"},
 			{field:"idphone", name: "idphone"},
 			{field:"phone", name: "phone"},
-			{field:"datesms", name: "datesms", width: "5%"},
+			{field:"datetosend", name: "datetosend", width: "5%"},
 			{field:"message", name: "message", width: "15%"},
+			{field:"dateprocess", name: "dateprocess", width: "5%"},
+			{field:"process", name: "process"},
+			{field:"priority", name: "priority"},
+			{field:"attempts", name: "attempts"},
+			{field:"idprovidersent", name: "idprovidersent"},
+			{field:"slices", name: "slices"},
+			{field:"slicessent", name: "slicessent"},
+			{field:"messageclass", name: "messageclass"},
+			{field:"report", name: "report"},
+			{field:"maxslices", name: "maxslices"},
+			{field:"enablemessageclass", name: "enablemessageclass"},
 			{field:"idport", name: "idport"},
-			{field:"status", name: "status"},
 			{field:"flag1", name: "flag1"},
 			{field:"flag2", name: "flag2"},
 			{field:"flag3", name: "flag3"},
 			{field:"flag4", name: "flag4"},
 			{field:"flag5", name: "flag5"},
+			{field:"retryonfail", name: "retryonfail"},
+			{field:"maxtimelive", name: "maxtimelive"},
 			{field:"note", name: "note", width: "10%"}
 		]);
 
@@ -62,64 +75,10 @@ myGridX.startup();
 
 }
 
-myGridX.Load= function(){
-            // Request the text file
-            request.get("view_smsin_datefilter.usms", {
-	query: {fstart: dijit.byId('fstart')._getDate(), fend: dijit.byId('fend')._getDate(), nrows: dijit.byId('nrows').get('value')},
-            handleAs: "xml"
-        }).then(
-                function(response){
-var d = new RXml.getFromXhr(response, 'row');
-var myData = {identifier: "unique_id", items: []};
-var i = 0;
-numrows = d.length;
-if(numrows > 0){
-while(i<numrows){
-myData.items[i] = {
-unique_id:i+1,
-idsmsin: d.getNumber(i, "idsmsin"), 
-dateload: d.getString(i, "dateload"),
-idprovider: d.getNumber(i, "idprovider"),
-idphone: d.getNumber(i, "idphone"),
-phone: d.getStringFromB64(i, "phone"),
-datesms: d.getString(i, "datesms"),
-message: d.getStringFromB64(i, "message"),
-idport: d.getNumber(i, "idport"),
-status: d.getNumber(i, "status"),
-flag1: d.getNumber(i, "flag1"),
-flag2: d.getNumber(i, "flag2"),
-flag3: d.getNumber(i, "flag3"),
-flag4: d.getNumber(i, "flag4"),
-flag5: d.getNumber(i, "flag5"),
-note: d.getStringFromB64(i, "note")
-};
-i++;
-}
-}
-	ItemFileReadStore_1.clearOnClose = true;
-	ItemFileReadStore_1.data = myData;
-	ItemFileReadStore_1.close();
-
-		myGridX.store = null;
-		myGridX.setStore(ItemFileReadStore_1);
-
-//myGridX.emit('onnotify', {msg: 'Se han cargado los datos'});
-
-                },
-                function(error){
-                    // Display the error returned
-myGridX.emit('onnotify', {msg: error});
-                }
-            );
-
-
-}
-
-
 
 function LoadGrid(){
 
-var store = new dojox.data.XmlStore({url: "view_smsin_datefilter.usms", sendQuery: true, rootItem: 'row'});
+var store = new dojox.data.XmlStore({url: "usms_smsoutviewtablefilter", sendQuery: true, rootItem: 'row'});
 
 var request = store.fetch({query: {fstart: getdate('fstart'), fend: getdate('fend'), nrows: dijit.byId('nrows').get('value')}, onComplete: function(itemsrow, r){
 
@@ -134,20 +93,33 @@ while(i<numrows){
 
 myData.items[i] = {
 unique_id:i, 
-idsmsin: dataxml.getNumber(i, "idsmsin"), 
+idsmsout: dataxml.getNumber(i, "idsmsout"), 
 dateload: dataxml.getString(i, "dateload"),
 idprovider: dataxml.getNumber(i, "idprovider"),
+idsmstype: dataxml.getNumber(i, "idsmstype"),
 idphone: dataxml.getNumber(i, "idphone"),
 phone: dataxml.getStringB64(i, "phone"),
-datesms: dataxml.getString(i, "datesms"),
+datetosend: dataxml.getString(i, "datetosend"),
 message: dataxml.getStringB64(i, "message"),
+dateprocess: dataxml.getString(i, "dateprocess"),
+process: dataxml.getNumber(i, "process"),
+priority: dataxml.getNumber(i, "priority"),
+attempts: dataxml.getNumber(i, "attempts"),
+idprovidersent: dataxml.getNumber(i, "idprovidersent"),
+slices: dataxml.getNumber(i, "slices"),
+slicessent: dataxml.getNumber(i, "slicessent"),
+messageclass: dataxml.getNumber(i, "messageclass"),
+report: dataxml.getBool(i, "report"),
+maxslices: dataxml.getNumber(i, "maxslices"),
+enablemessageclass: dataxml.getBool(i, "enablemessageclass"),
 idport: dataxml.getNumber(i, "idport"),
-status: dataxml.getNumber(i, "status"),
 flag1: dataxml.getNumber(i, "flag1"),
 flag2: dataxml.getNumber(i, "flag2"),
 flag3: dataxml.getNumber(i, "flag3"),
 flag4: dataxml.getNumber(i, "flag4"),
 flag5: dataxml.getNumber(i, "flag5"),
+retryonfail: dataxml.getNumber(i, "retryonfail"),
+maxtimelive: dataxml.getNumber(i, "maxtimelive"),
 note: dataxml.getStringB64(i, "note")
 };
 i++;
@@ -172,6 +144,11 @@ alert(e);
 // Se hace este timeout porque la pagina demora en crearse y al cargar no muestra nada.
 //setTimeout(LoadGrid, 5000);
 
+
+
+function getdate(iddijit){
+return dojo.date.locale.format(dijit.byId(iddijit).get('value'), {datePattern: "yyyy-MM-dd", selector: "date"});
+}
 
 
 
