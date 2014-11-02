6225ac934626672dbf3d1beecaad234ca593cb0d
edwinspire <edwinspire@gmail.com>
2014-11-01 19:25:39 -0500

    Enviando cambio

--- bin/lnx/run.sh
+++ bin/lnx/run.sh
@@ -2,5 +2,5 @@
 
 export LD_LIBRARY_PATH=.
 
-export LD_LIBRARY_PATH="/home/edwinspire/programacion/ProyectosSoftware/Software Vala/ProyectosVala/proyectlibs/lnx"
-./uSAGA
+export LD_LIBRARY_PATH="/home/edwinspire/Documentos/DesarrolloSoftware/ProyectosSoftware_ng/proyectlibs/lnx"
+./usaga
